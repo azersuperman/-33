dc2dccdbd382ce38b5a78494e17274864f730128
zhangkx <1099253732@qq.com>
2022-09-11 10:22:42 +0800

    feat:我的页面完成终于

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
 // 引入axios
 // 配置axios
 import axios from 'axios'
-import { store } from '@/store'
+import store from '@/store'
 // axios.defaults.baseURL = 'http://toutiao.itheima.net'
 // axios.defaults.timeout = 3000
 
